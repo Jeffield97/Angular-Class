cdd2c5e2b9869e7c3bf38a56ca010dfd33bde271
Jeffield97 <jchariguaman@gmail.com>
2023-11-09 07:36:17 -0500

    Event binding with input

--- src/app/list-container/list-container.component.ts
+++ src/app/list-container/list-container.component.ts
@@ -6,12 +6,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./list-container.component.css']
 })
 export class ListContainerComponent {
+  // Properties
   name: string = "Jeffield"
   lastname: string = "Fullbuster"
   age: number = 26
   btn_disable: boolean = true
   message = "Any person was be added"
+  title=""
+  //Functions
   addTask():void {
     this.message= "Task added successfully"
+  }
+  changeTitle(e:Event): void{
+    this.title=(<HTMLInputElement>e.target).value
   }
 }
